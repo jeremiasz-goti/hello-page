072985fdf02fb58b9d42bc36fad923f49b7f885c
jeremiasz-goti <jeremiasz.goti@gmail.com>
2022-12-27 21:11:14 +0100

    basic js for hiding sections

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+//  console.log('hello');
+ 
+//  const observer = new IntersectionObserver((entries) => {
+//      console.log(entries)
+//      entries.forEach((entry) => {
+//          console.log(entry)
+//          if (entry.isIntersecting) {
+//              entry.target.classList.add('show');
+//          } 
+//          else {
+//             entry.target.classList.remove('show');
+//          }
+
+//      });
+//  });
+
+
+
+// const hide = document.querySelectorAll('hide');
+// console.log(hide);
+// hide.forEach((el) => observer.observe(el));
